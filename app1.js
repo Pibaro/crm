2aff412aa0a036198fcb71e92fd1d11246532427
Baraka Pilipili <Pibaro>
2020-01-31 22:36:45 +0000

    updated server

--- app1.js
+++ app1.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
 	app1.use(express.static('client/build'));
 }
 
-app1.get('*', (req, res)=>{
+app1.get('/', (req, res)=>{
   res.sendFile(path.join('index.html', { root: 'C:/Users/barak/Desktop/crm' }));
 })
 
