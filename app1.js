64421223f540bfad8cb91a891de5ad5c859d8a78
Baraka Pilipili <Pibaro>
2020-01-31 23:13:22 +0000

    updated server

--- app1.js
+++ app1.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app1.get('/', (req, res)=>{
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 app1.get("/data", (req, res)=>{
